208e7b27f06d7973a23dcc97e302f52c504f5e41
Valentin Minkov <minkowalentin@proton.me>
2024-06-04 16:20:23 +0200

    feat: add persistent session storage, install nanostores

---
+++ src/stores/userStore.ts
@@ -0,0 +1,53 @@
+import { persistentMap } from "@nanostores/persistent";
+
+export interface UserFormValue {
+  firstName: string;
+  secondName: string;
+  email: string;
+  nationality: string;
+  phoneNumber: string;
+  website: string;
+  addressType: string;
+  addressLine1: string;
+  addressLine2: string;
+  postCode: string;
+  gender: string;
+  city: string;
+  country: string;
+  aboutMe?: string;
+  dateOfBirth?: Date | string;
+}
+
+type PersistentUserFormValue = UserFormValue &
+  Record<string, string | undefined>;
+
+const initialState: PersistentUserFormValue = {
+  firstName: "",
+  secondName: "",
+  phoneNumber: "",
+  gender: "",
+  nationality: "",
+  website: "",
+  addressType: "",
+  addressLine1: "",
+  addressLine2: "",
+  postCode: "",
+  city: "",
+  country: "",
+  aboutMe: "",
+  email: "",
+};
+
+export const $user = persistentMap<PersistentUserFormValue>(
+  "user_",
+  initialState
+);
+
+export function updateUserData(newData: Partial<UserFormValue>) {
+  const formattedUserData = {
+    ...newData,
+    dateOfBirth: newData.dateOfBirth?.toString(),
+  };
+
+  $user.set({ ...$user.get(), ...formattedUserData });
+}
