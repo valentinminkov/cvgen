209be6d65a7b117725cc959c31dbf769e2b3ae55
Valentin Minkov <minkowalentin@proton.me>
2024-06-05 23:34:05 +0200

    work experiences store

---
+++ src/stores/experienceStore.ts
@@ -0,0 +1,43 @@
+import { persistentMap } from "@nanostores/persistent";
+
+export interface ExperienceFormValue {
+  title: string;
+  country: string;
+  workDescription: string;
+  startDate: Date;
+  ongoing?: boolean;
+  endDate?: Date;
+}
+
+type PersistentExperienceStore = Record<string, any> & {
+  experiences: ExperienceFormValue[];
+};
+
+const storeExperiences = persistentMap<PersistentExperienceStore>(
+  "work_experiences",
+  { experiences: [] },
+  {
+    encode: JSON.stringify,
+    decode: JSON.parse,
+  }
+);
+
+function addExperience(newEntry: ExperienceFormValue) {
+  const state = storeExperiences.get();
+
+  state.experiences.push(newEntry);
+
+  storeExperiences.set({ experiences: state.experiences });
+
+  return true;
+}
+
+function removeExperience(index: number) {
+  const newItems = storeExperiences
+    .get()
+    .experiences.filter((_, i) => i !== index);
+
+  storeExperiences.set({ experiences: newItems });
+}
+
+export { storeExperiences, addExperience, removeExperience };
