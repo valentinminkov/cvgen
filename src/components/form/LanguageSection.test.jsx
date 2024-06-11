86c76794bfb38abcfc11f60b440a4f1afc65b35a
Valentin Minkov <minkowalentin@proton.me>
2024-06-11 09:37:58 +0200

    test: educaiton input unit test

---
+++ src/components/form/LanguageSection.test.jsx
@@ -0,0 +1,52 @@
+import { render, screen } from "@testing-library/react";
+import LanguageSection from "@/components/form/LanguageSection";
+import { expect, test } from "vitest";
+import userEvent from "@testing-library/user-event";
+import { content } from "@/config/content";
+
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+const {
+  content: {
+    components: {
+      LanguageSection: {
+        MOTHER_LANGUAGES,
+        LANGUAGE,
+        LISTENING,
+        READING,
+        WRITING,
+        SUBMIT,
+        ENTRIES,
+      },
+    },
+  },
+} = content;
+
+test("Experience Section", async function () {
+  window.ResizeObserver = ResizeObserver;
+
+  render(<LanguageSection />);
+
+  const motherLanguageInput = screen.getByText(MOTHER_LANGUAGES);
+  const languageInput = screen.getByText(LANGUAGE);
+  const listeningInput = screen.getByText(LISTENING);
+  const readingInput = screen.getByText(READING);
+  const writingInput = screen.getByText(WRITING);
+  const submitButton = screen.getByText(SUBMIT);
+
+  expect(screen.queryByText(ENTRIES)).not.toBeInTheDocument;
+  await userEvent.type(motherLanguageInput, "Mandarin");
+  await userEvent.type(languageInput, "German");
+  await userEvent.type(listeningInput, "A2");
+  await userEvent.type(readingInput, "B1");
+  await userEvent.click(writingInput, "C1");
+  await userEvent.click(submitButton);
+  expect(screen.queryByText(ENTRIES)).exist;
+  expect(screen.findByText("Mandarin")).exist;
+  await userEvent.type(motherLanguageInput, "Hindi");
+  expect(screen.findByText("Hindi")).exist;
+});
