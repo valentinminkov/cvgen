ca29acdefd3e8222ee19a0590c40bd7fb20487b5
Valentin Minkov <minkowalentin@proton.me>
2024-06-26 00:23:34 +0200

    feat:util for loading theme components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
+import type { ThemeComponents } from "@/types";
 import { type ClassValue, clsx } from "clsx";
+import { lazy } from "react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,3 +13,39 @@ export const getRandomString = (length: number) =>
       Math.floor(Math.random() * 62)
     )
   ).join("");
+
+// TODO Make it dynamic
+export const loadThemeComponents = async (
+  theme: string
+): Promise<ThemeComponents> => {
+  const themeComponents: ThemeComponents = {
+    personal: null,
+    experiences: null,
+    educations: null,
+    language: null,
+    skills: null,
+  };
+  switch (theme) {
+    case "default":
+      themeComponents.personal = lazy(
+        () => import(`@/components/theme/themes/default/personal`)
+      );
+      themeComponents.experiences = lazy(
+        () => import(`@/components/theme/themes/default/experience`)
+      );
+      themeComponents.educations = lazy(
+        () => import(`@/components/theme/themes/default/education`)
+      );
+      themeComponents.skills = lazy(
+        () => import(`@/components/theme/themes/default/skills`)
+      );
+      themeComponents.language = lazy(
+        () => import(`@/components/theme/themes/default/languages`)
+      );
+      break;
+    default:
+      break;
+  }
+
+  return themeComponents;
+};
