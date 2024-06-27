f06138960dcc40123c2bd05e0ca3b1325a8cc4c6
Valentin Minkov <minkowalentin@proton.me>
2024-06-27 23:39:19 +0200

    add shuffleString util for keys

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -50,3 +50,17 @@ export const loadThemeComponents = async (
 
   return themeComponents;
 };
+
+export function shuffleString(str: string) {
+  // Convert the string into an array of characters
+  let arr = str.split("");
+
+  // Shuffle the array using Fisher-Yates algorithm
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+  }
+
+  // Convert the array back to a string
+  return arr.join("");
+}
