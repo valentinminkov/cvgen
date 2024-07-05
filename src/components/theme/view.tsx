c65b49191f5935a601f1f5372007a9d42c4006a8
valentinminkov <minkowalentin@proton.me>
2024-07-05 21:08:27 +0200

    wip

--- src/components/theme/view.tsx
+++ src/components/theme/view.tsx
@@ -3,7 +3,7 @@
 import { loadThemeComponents } from "@/lib/utils";
 import { $settings } from "@/stores/settingsStore";
 import { $allEntriesSorted, $allSections } from "@/stores/computed";
-import type { ThemeComponents } from "@/types";
+import type { Sections, ThemeComponents } from "@/types";
 import { useStore } from "@nanostores/react";
 import { Suspense, useEffect, useRef, useState } from "react";
 import Loading from "@/components/Loading";
@@ -12,6 +12,8 @@ import html2PDF from "jspdf-html2canvas";
 import type { MouseEvent } from "react";
 import { toast } from "@/components/ui/use-toast";
 import { translations } from "@/config/content";
+
+const A4_PAGE_HEIGHT_PX = 1500;
 
 function LoadingComponent({ componentName }: { componentName: string }) {
   return <div>Loading {componentName} data component</div>;
@@ -25,13 +27,21 @@ function ComponentNotAvailable({ componentName }: { componentName?: string }) {
   );
 }
 
+const classes = {
+  pageContainer: "",
+  page: "pb-4",
+};
+
 export default function View() {
   const { theme, darkMode } = useStore($settings);
   const sections = useStore($allSections);
   const [components, setComponents] = useState<ThemeComponents | null>(null);
   const [loading, setLoading] = useState(true);
   const entries = useStore($allEntriesSorted);
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const sectionRefs = useRef<HTMLDivElement[]>([]);
+  const pageRefs = useRef<HTMLDivElement[]>([]);
+  const [pagesHTML, setPagesHTML] = useState<JSX.Element[]>([]);
 
   useEffect(() => {
     const loadComponents = async () => {
@@ -42,6 +52,97 @@ export default function View() {
 
     loadComponents();
   }, [theme]);
+
+  // load pages html based on section component height
+  // which is rendered before the 'pages'
+  useEffect(() => {
+    if (containerRef.current) {
+      //  ensure that the height of elements is captured after they have been fully rendered
+      const observer = new MutationObserver(() => {
+        if (sectionRefs.current.length === sections.length) {
+          const htmlPagesElements = formatPages();
+          setPagesHTML(htmlPagesElements);
+        }
+      });
+
+      observer.observe(containerRef.current, {
+        childList: true,
+        subtree: true,
+      });
+
+      return () => observer.disconnect();
+    }
+  }, [sections, entries, components]);
+
+  const generatePDF = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (!pageRefs?.current.length) {
+      toast({
+        title: translations.CV_GENERATE_FAIL,
+      });
+      return;
+    }
+    try {
+      await html2PDF(pageRefs.current, {
+        jsPDF: { format: "a4" },
+        success: (pagesPdf) => {
+          pagesPdf.save();
+        },
+      });
+    } catch (error) {
+      console.log("error generating pdf", error);
+    }
+  };
+
+  const formatPages = () => {
+    const newPages: JSX.Element[] = [];
+    let currentPageHeight = 0;
+    let currentPageElements: JSX.Element[] = [];
+
+    sectionRefs.current?.forEach((sectionRef, i) => {
+      if (sectionRef) {
+        const sectionHeight = sectionRef.offsetHeight;
+        const sectionHtml = getSectionHtml(sections[i], i);
+        if (currentPageHeight + sectionHeight > A4_PAGE_HEIGHT_PX) {
+          // Complete the current page and start a new one
+          newPages.push(
+            <div
+              className={`page-${newPages.length + 1} ${classes.page}`}
+              key={newPages.length}
+              ref={(newPageRef) => {
+                if (newPageRef) pageRefs.current.push(newPageRef);
+              }}
+            >
+              {currentPageElements}
+            </div>
+          );
+          currentPageElements = [sectionHtml];
+          currentPageHeight = sectionHeight;
+        } else {
+          // Add to the current page
+          currentPageElements.push(sectionHtml);
+          currentPageHeight += sectionHeight;
+        }
+      }
+    });
+
+    if (currentPageElements.length) {
+      newPages.push(
+        <div
+          className={`page-${newPages.length + 1} ${classes.page}`}
+          key={newPages.length}
+          ref={(newPageRef) => {
+            if (newPageRef) pageRefs.current.push(newPageRef);
+          }}
+        >
+          {currentPageElements}
+        </div>
+      );
+    }
+
+    return newPages;
+  };
 
   if (loading) {
     return <Loading />;
@@ -82,16 +183,28 @@ export default function View() {
     },
   };
 
-  const generatePDF = async (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  const getSectionHtml = (section: Sections, index?: number) => {
+    const Component = componentMapping[section];
+    const componentProps = componentPropsMapping[section];
 
-    if (!containerRef?.current) {
-      toast({
-        title: translations.CV_GENERATE_FAIL,
-      });
-      return;
-    }
-    await html2PDF(containerRef?.current, {});
+    return (
+      <div
+        key={section}
+        ref={(curRef) => {
+          if (index !== undefined && curRef) {
+            sectionRefs.current[index] = curRef;
+          }
+        }}
+      >
+        {Component ? (
+          <Suspense fallback={<LoadingComponent componentName={section} />}>
+            <Component {...componentProps} />
+          </Suspense>
+        ) : (
+          <ComponentNotAvailable componentName={section} />
+        )}
+      </div>
+    );
   };
 
   return (
@@ -99,26 +212,16 @@ export default function View() {
       <GeneratePdf
         onClick={(e: MouseEvent<HTMLButtonElement>) => generatePDF(e)}
       />
-      <div className="flex flex-col" ref={containerRef}>
-        {sections.map((section) => {
-          const Component = componentMapping[section];
-          const componentProps = componentPropsMapping[section];
 
-          return (
-            <div key={section}>
-              {Component ? (
-                <Suspense
-                  fallback={<LoadingComponent componentName={section} />}
-                >
-                  <Component {...componentProps} />
-                </Suspense>
-              ) : (
-                <ComponentNotAvailable componentName={section} />
-              )}
-            </div>
-          );
-        })}
-      </div>
+      {!pagesHTML.length && (
+        <div ref={containerRef}>
+          <div className="flex flex-col page">
+            {sections.map((section, index) => getSectionHtml(section, index))}
+          </div>
+        </div>
+      )}
+
+      {pagesHTML.length && pagesHTML}
     </div>
   );
 }
